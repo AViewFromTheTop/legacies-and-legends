3ab30a7a0b684902bfd3a267ee4ff33ab804855d
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-01-05 18:02:20 +0100

    armor fixes

--- src/main/java/net/legacy/legacies_and_legends/LegaciesArmorMaterials.java
+++ src/main/java/net/legacy/legacies_and_legends/LegaciesArmorMaterials.java
@@ -7,7 +7,7 @@ import net.minecraft.world.item.equipment.ArmorType;
 import java.util.EnumMap;
 
 public interface LegaciesArmorMaterials {
-    net.minecraft.world.item.equipment.ArmorMaterial DUNGEON = new net.minecraft.world.item.equipment.ArmorMaterial(5, Util.make(new EnumMap(ArmorType.class), enumMap -> {
+    net.minecraft.world.item.equipment.ArmorMaterial DUNGEON = new net.minecraft.world.item.equipment.ArmorMaterial(27, Util.make(new EnumMap(ArmorType.class), enumMap -> {
         enumMap.put(ArmorType.BOOTS, 2);
         enumMap.put(ArmorType.LEGGINGS, 5);
         enumMap.put(ArmorType.CHESTPLATE, 6);
@@ -15,21 +15,21 @@ public interface LegaciesArmorMaterials {
         enumMap.put(ArmorType.BODY, 5);
     }), 10, SoundEvents.ARMOR_EQUIP_DIAMOND, 0.0F, 0.1F, LegaciesItemTags.REPAIRS_DUNGEON_ARMOR, LegaciesEquipmentAssets.DUNGEON);
 
-    net.minecraft.world.item.equipment.ArmorMaterial FORTRESS = new net.minecraft.world.item.equipment.ArmorMaterial(5, Util.make(new EnumMap(ArmorType.class), enumMap -> {
+    net.minecraft.world.item.equipment.ArmorMaterial FORTRESS = new net.minecraft.world.item.equipment.ArmorMaterial(23, Util.make(new EnumMap(ArmorType.class), enumMap -> {
         enumMap.put(ArmorType.BOOTS, 3);
         enumMap.put(ArmorType.LEGGINGS, 6);
         enumMap.put(ArmorType.CHESTPLATE, 6);
         enumMap.put(ArmorType.HELMET, 3);
         enumMap.put(ArmorType.BODY, 7);
-    }), 10, SoundEvents.ARMOR_EQUIP_DIAMOND, 0.0F, 0.1F, LegaciesItemTags.REPAIRS_FORTRESS_ARMOR, LegaciesEquipmentAssets.FORTRESS);
+    }), 16, SoundEvents.ARMOR_EQUIP_DIAMOND, 0.0F, 0.1F, LegaciesItemTags.REPAIRS_FORTRESS_ARMOR, LegaciesEquipmentAssets.FORTRESS);
 
-    net.minecraft.world.item.equipment.ArmorMaterial WARD = new net.minecraft.world.item.equipment.ArmorMaterial(5, Util.make(new EnumMap(ArmorType.class), enumMap -> {
+    net.minecraft.world.item.equipment.ArmorMaterial WARD = new net.minecraft.world.item.equipment.ArmorMaterial(41, Util.make(new EnumMap(ArmorType.class), enumMap -> {
         enumMap.put(ArmorType.BOOTS, 3);
         enumMap.put(ArmorType.LEGGINGS, 6);
         enumMap.put(ArmorType.CHESTPLATE, 7);
         enumMap.put(ArmorType.HELMET, 4);
         enumMap.put(ArmorType.BODY, 11);
-    }), 10, SoundEvents.ARMOR_EQUIP_DIAMOND, 1.0F, 0.2F, LegaciesItemTags.REPAIRS_WARD_ARMOR, LegaciesEquipmentAssets.WARD);
+    }), 8, SoundEvents.ARMOR_EQUIP_DIAMOND, 1.0F, 0.2F, LegaciesItemTags.REPAIRS_WARD_ARMOR, LegaciesEquipmentAssets.WARD);
 }
 
 
