a6a7df4d60d21de109a915839134983bedcf6663
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-07-27 01:22:37 +1000

    Update CleavingBattleAxeItem.java

--- src/main/java/net/legacy/legacies_and_legends/item/CleavingBattleAxeItem.java
+++ src/main/java/net/legacy/legacies_and_legends/item/CleavingBattleAxeItem.java
@@ -1,17 +1,12 @@
 package net.legacy.legacies_and_legends.item;
 
 import net.legacy.legacies_and_legends.registry.LaLToolMaterial;
-import net.minecraft.ChatFormatting;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.*;
-import net.minecraft.world.item.component.TooltipDisplay;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.function.Consumer;
 
 public class CleavingBattleAxeItem extends AxeItem {
 
@@ -23,5 +18,4 @@ public class CleavingBattleAxeItem extends AxeItem {
     public void postHurtEnemy(@NotNull ItemStack stack, @NotNull LivingEntity target, LivingEntity attacker) {
         target.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 100, 0), attacker);
     }
-
 }
